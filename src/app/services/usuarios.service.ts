95f1b8b8947e4bd3ec3c1e094c38ae2e4ee49330
Inés García <inesgarciadiezgd@gmail.com>
2023-06-01 16:15:50 +0200

    [MOD] Improvements token

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +8,32 @@ import { firstValueFrom } from 'rxjs';
 export class UsuariosService {
 
   private baseUrl: string
+  private loggedIn: boolean;
+
+  private _logged: BehaviorSubject<boolean>;
 
   constructor(private httpClient: HttpClient) { 
-    this.baseUrl = 'http://localhost:3000/api/usuarios'
+    this.baseUrl = 'http://localhost:3000/api/usuarios';
+    this.loggedIn = localStorage.getItem('token_user') ? true : false;
+    this._logged = new BehaviorSubject(this.loggedIn);
+  }
+
+  get logged() {
+    return this._logged.asObservable();
   }
 
   login(values: { email: string, password: string}) {
     return firstValueFrom (
       this.httpClient.post<any>(`${this.baseUrl}/login`, values)
     )
+  }
+
+  isLogged(): boolean {
+    return localStorage.getItem('token_gym') ? true : false;
+  }
+
+  changeLogin(logged: boolean) {
+    this._logged.next(logged);
   }
 
 }
